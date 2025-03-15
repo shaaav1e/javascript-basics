54cf41fd59a9c06acff7baf52a702fb46b9eab8c
shaaav1e <shaaavie@gmail.com>
2025-03-15 17:44:23 +0500

    folder structure updated

---
+++ 01_Basics/ternary.js
@@ -0,0 +1,10 @@
+// Condition ? expression1 : expression2
+// shorthand way to write if else
+
+const age = 24;
+const vote = age >= 18 ? "You can vote" : "You can't vote";
+console.log(vote);
+
+const num = 7;
+const result = num % 2 === 0 ? "Even number" : "Odd number";
+console.log(result);
